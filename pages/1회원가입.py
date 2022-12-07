0ccc5744da26538050545b7aefe0d698047cd9fb
bullbearbull <115724211+bullbearbull@users.noreply.github.com>
2022-12-07 16:06:15 +0900

    bovl1

--- pages/1회원가입.py
+++ pages/1회원가입.py
@@ -16,6 +16,9 @@ def main() :
   st.title('👋 나만의 요리사 👋')
   st.subheader('회원가입을 도와드리겠습니다')
   st.subheader('사용에 어려움이 있으시면 옆에 있는 직원에게 이야기해주세요')
+  st.subheader('언제든 편하게 말씀해주세요!')
+  st.write('최초 가입 고객 한정')
+  st.write('3회 식사 이용권 증정')
 
   i_name = st.text_input('이름을 입력해주세요 :')
   i_birth = st.date_input('생년월일을 입력해주세요 : (예시 601020')
@@ -29,12 +32,14 @@ def main() :
   if i_password == i_password_check:
     pass
   else: st.error("비밀번호가 일치하지 않습니다")
-
+  i_secure = pd.DataFrame({'name':[i_name], 'birth':[i_birth], 'password' : [i_password]})
   i_data = pd.DataFrame({'name': [i_name], 'birth': [i_birth], 'sex': [i_sex], 'disease': [i_disease], 'phone_num':[i_number]})
   i_data.set_index('name', inplace=True)
+  i_secure.set_index('name', inplace=True)
   checkbox_btn = st.checkbox('입력완료')
   if checkbox_btn :
     i_data.to_csv(f'ID_DB/{i_name}{i_birth}.csv')
+    i_secure.to_csv(f'Secure_DB/{i_name}{i_birth}.csv')
     st.success('가입이 완료되었습니다.')
     time.sleep(2)
 
--- pages/2결제.py
+++ pages/2결제.py
@@ -1,10 +1,39 @@
+import pandas as pd
 import streamlit as st
 
-st.set_page_config(
+def main() :
+  st.set_page_config(
     page_icon="👋",
     page_title="나만의 요리사",
     layout="wide")
 
-st.header('결제')
+  st.title('👋 나만의 요리사 👋')
+  st.subheader('결제하시겠습니까?')
+  st.subheader('사용에 어려움이 있으시면 옆에 있는 직원에게 이야기해주세요')
+  st.subheader('언제든 편하게 말씀해주세요!')
+  i_name = st.text_input('이름을 입력해주세요 :')
+  i_birth = st.date_input('생년월일을 입력해주세요 : (예시 601020')
+  checkbox_btn1 = st.checkbox('입력완료!')
+  if checkbox_btn1 :
+    i_secure = pd.DataFrame(pd.read_csv(f'Secure_DB/{i_name}{i_birth}.csv')).iloc[0,2].astype(str)
+    st.write(i_secure)
+    i_password = st.text_input('비밀번호를 입력해주세요 :', type="password")
+    checkbox_btn2 = st.checkbox('입력완료')
+    if checkbox_btn2 :
+      if i_secure == i_password:
+        st.header('종량제 식권 구매')
+        st.write('서비스 소개 : 원하시는 때에 식사를 하실 수 있습니다!')
+        st.write('이용기한 : 3개월')
+        st.write('1회 이용권 : 7000원')
+        st.write('10회 이용권 : 60000원')
+        st.write('30회 이용권 : 160000원')
 
-st.write
+        st.write('')
+        st.header('기간제 식권 구매')
+        st.header('서비스 소개 : 식사를 포함해 정기적인 ')
+
+      else : st.error('비밀번호가 일치하지 않습니다.')
+
+
+
+main()
