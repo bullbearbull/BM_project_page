c1e72ccb2cfc07058cfc3566c05b2f229cb81c14
bullbearbull <115724211+bullbearbull@users.noreply.github.com>
2022-12-08 04:29:39 +0900

    Changes

--- pages/9관리자.py
+++ pages/9관리자.py
@@ -11,7 +11,11 @@ warnings.filterwarnings('ignore')
 
 
 def main() :
-
+  password = 1234567890
   st.title('고객 관리 계정')
+  i_pass = st.number_input('관리자 비밀번호를 입력해주세요 :', 0, 9999999999)
+  if i_pass == password :
+    st.success('관리자 로그인 성공')
 
+  else : st.error('관리자 로그인 실패')
 main()
